cfcff2a414c6e7cac047ff8aa93441a3a2113f63
volha_karpuk <volha.karpuk10@gmail.com>
2024-10-20 21:19:05 +0300

    added test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,26 @@
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.qameta.allure.Allure.step;
+
+public class SimpleTest {
+
+    @ValueSource(ints = {1 , 2 , 3, 4})
+    @ParameterizedTest(name = "Проверяем, что {0} меньше 5")
+    public void test1(int number){
+        Assertions.assertTrue(number < 5);
+    }
+
+    @ValueSource(strings = {"one", "two" ,"three", "four"})
+    @ParameterizedTest(name = "Проверяем, что количество символов в слове {0} меньше 5")
+    public void test2(String number){
+        Integer numberOfLetters = number.length();
+        step("Проверяем, что количество символов в слове", () ->{
+            Assertions.assertTrue(numberOfLetters < 5);
+        });
+    }
+}
